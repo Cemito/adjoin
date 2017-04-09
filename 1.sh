86dbe20482654e01f3362e33c683ceb6e7e8be77
Cemito <cemsalih@gmail.com>
2017-04-09 21:43:48 +0100

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -2,7 +2,7 @@
 sudo apt-get install git
 git clone https://github.com/Cemito/adjoin.git
 sleep 3
-alias changedir="cd adjoin"
+alias changedir="cd /adjoin"
 changedir
 chmod +x u_corpjoin datadrive.sh
 ./u_corpjoin
